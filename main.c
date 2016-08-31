b07cf9d2aac933aaeffaf833fddfb64f5b7f5457
Agelb <ajgelber@gmail.com>
2016-08-31 01:27:20 -0500

    Basic additive synthesis

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
     char* FILENAME = 0;
     const unsigned short MAX_FILENAME_SIZE;
-    float seconds = 2;
+    float seconds = 4.0;
     int i;
     for(i=0; i < argc; ++i){
         if(strcmp("-f", argv[i]) == 0) {
@@ -37,19 +37,52 @@ int main(int argc, char *argv[])
     InitializeWaveFile(&wFile);
     SetFilename(FILENAME, &wFile);
     SetBitRate(44100, &wFile);
-    SetChannelCount(2, &wFile);
+    SetChannelCount(1, &wFile);
+    long frame_index;
+    frame* signal_a;
+    frame* signal_b;
+    AllocateFrames(&signal_a, (long)seconds * 44100, 16, 1);
+    AllocateFrames(&signal_b, (long)seconds * 44100, 16, 1);
+
+    for(frame_index = 0; frame_index < (long)seconds * wFile.fChunk.dwSamplesPerSec; frame_index++) {
+        AllocateFrame(&signal_a[frame_index],16, 1);
+        AllocateFrame(&signal_b[frame_index],16, 1);
+    }
+
+    short max_amplitude = 32600;
+    GenerateSineWave_Mono_16a(signal_a, 440.0, seconds, max_amplitude/7, &wFile.fChunk);
+    GenerateSineWave_Mono_16a(signal_b, 523.25, seconds, max_amplitude/7, &wFile.fChunk);
+
 
     AllocateFrames(&wFile.dChunk.frames, (long)seconds * wFile.fChunk.dwSamplesPerSec, wFile.fChunk.dwBitsPerSample, wFile.fChunk.wChannels);
-    long frame_index;
+
     for(frame_index = 0; frame_index < (long)seconds * wFile.fChunk.dwSamplesPerSec; frame_index++) {
         AllocateFrame(&wFile.dChunk.frames[frame_index],16, 1);
     }
-    //AllocateFrame(&wFile.dChunk.frames[0],16, 1);
-    //memset(wFile.dChunk.shortArray, '0', seconds * wFile.fChunk.dwSamplesPerSec * sizeof(short));
-    //sample testSample;
-    //testSample.value = 9090;
-    //WriteToFrame(&testSample,&wFile.dChunk.frames[0],1,16);
-    mGenerate16SineWave(wFile.dChunk.frames, 441.5, seconds, 2, &wFile.fChunk);
+
+
+    Add_Waves_Mono_16(wFile.dChunk.frames,signal_a, signal_b, 44100 * (long)seconds);
+    Copy_Frames(signal_b, wFile.dChunk.frames, 44100 * (long)seconds);
+
+    GenerateSineWave_Mono_16a(signal_a, 659.25, seconds, max_amplitude/7, &wFile.fChunk);
+    Add_Waves_Mono_16(wFile.dChunk.frames, signal_a, signal_b, 44100 * (long)seconds);
+
+    Copy_Frames(signal_b, wFile.dChunk.frames, 44100 * (long)seconds);
+    GenerateSineWave_Mono_16a(signal_a, 174.61, seconds, max_amplitude/7, &wFile.fChunk);
+    Add_Waves_Mono_16(wFile.dChunk.frames, signal_a, signal_b, 44100 * (long)seconds);
+
+    Copy_Frames(signal_b, wFile.dChunk.frames, 44100 * (long)seconds);
+    GenerateSineWave_Mono_16a(signal_a, 392.00, seconds, max_amplitude/7, &wFile.fChunk);
+    Add_Waves_Mono_16(wFile.dChunk.frames, signal_a, signal_b, 44100 * (long)seconds);
+
+    Copy_Frames(signal_b, wFile.dChunk.frames, 44100 * (long)seconds);
+    GenerateSineWave_Mono_16a(signal_a, 1318.51, seconds, max_amplitude/7, &wFile.fChunk);
+    Add_Waves_Mono_16(wFile.dChunk.frames, signal_a, signal_b, 44100 * (long)seconds);
+
+    Copy_Frames(signal_b, wFile.dChunk.frames, 44100 * (long)seconds);
+    GenerateSineWave_Mono_16a(signal_a, 43.65, seconds, max_amplitude/7, &wFile.fChunk);
+    Add_Waves_Mono_16(wFile.dChunk.frames, signal_a, signal_b, 44100 * (long)seconds);
+
     wFile.dChunk.header.dwChunkSize = seconds * wFile.fChunk.dwAvgBytesPerSec;
 
     wFile.rHeader.dwFileLength = GetFileSize(&wFile);
--- synthesizer.c
+++ synthesizer.c
@@ -7,10 +7,24 @@
     params: destination, frequency in Hz, duration in seconds, format_chunk from wave file
 */
 // TODO: decouple this from the wave format
-void Generate16SineWave(frame* dest, const double frequency, const double duration, const format_chunk* format) {
+void GenerateSineWave_Mono_16(frame* dest, const double frequency, const double duration, const format_chunk* format) {
     // TODO: variable amplitudes
     // TODO: amplitude based on bit depth
-    short amplitude = 32760; // Max amplitude for 16 bit sound
+    //short amplitude = 327603/8; // Max amplitude for 16 bit sound
+    short amplitude = 5800;
+    float angle = (M_PI * 2 * frequency) / (format->dwSamplesPerSec * format->wChannels);
+    unsigned long total_frames = duration * format->dwSamplesPerSec;
+    unsigned long index;
+    sample iterated_sample;
+    for(index = 0; index < total_frames; index++)
+    {
+        iterated_sample.value = (short)(amplitude * sin(angle*index));
+        WriteToFrame(&iterated_sample,&dest[index],1,format->dwBitsPerSample);
+    }
+}
+
+void GenerateSineWave_Mono_16a(frame* dest, const double frequency, const double duration, const unsigned short amplitude, const format_chunk* format) {
+    // TODO: amplitude based on bit depth
     float angle = (M_PI * 2 * frequency) / (format->dwSamplesPerSec * format->wChannels);
     unsigned long total_frames = duration * format->dwSamplesPerSec;
     unsigned long index;
@@ -25,7 +39,7 @@ void Generate16SineWave(frame* dest, const double frequency, const double durati
 void mGenerate16SineWave(frame* dest, const double frequency, const double duration, const int num_channels, const format_chunk* format) {
     // TODO: variable amplitudes
     // TODO: amplitude based on bit depth
-    short amplitude = 32760; // Max amplitude for 16 bit sound
+    short amplitude = 32760/4; // Max amplitude for 16 bit sound
     float angle = (M_PI * 2 * frequency) / (format->dwSamplesPerSec * format->wChannels);
     unsigned long total_frames = duration * format->dwSamplesPerSec;
     unsigned long index;
@@ -43,5 +57,24 @@ void mGenerate16SineWave(frame* dest, const double frequency, const double durat
 
 void Add_Waves_Mono_16(frame* dest, const frame* signal_a, const frame* signal_b, const unsigned long num_samples) {
     unsigned long frame_index = 0;
+    long overflow_detection;
+    sample iterated_sample;
+    for(frame_index = 0; frame_index < num_samples; frame_index++) {
+        overflow_detection = signal_a[frame_index].samples[0].value + signal_b[frame_index].samples[0].value;
+        if(overflow_detection > 32600) {
+            iterated_sample.value = 32600;
+        } else if (overflow_detection < -32600) {
+            iterated_sample.value = -32600;
+        } else
+            iterated_sample.value = (short)overflow_detection;
+        WriteToFrame(&iterated_sample, &dest[frame_index],1,16);
+    }
+}
 
+//TODO: multi-channel frame copy
+void Copy_Frames(frame* dest, const frame* src, const unsigned long num_frames) {
+    unsigned long frame_index;
+    for(frame_index = 0; frame_index < num_frames; frame_index++) {
+        dest[frame_index].samples[0].value = src[frame_index].samples[0].value;
+    }
 }
